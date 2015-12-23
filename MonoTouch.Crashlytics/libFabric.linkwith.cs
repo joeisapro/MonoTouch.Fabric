164c7d7fb990ea579c3de5b51ea4249ba13c4aee
joeisapro <joeisapro@users.noreply.github.com>
2015-12-23 17:49:08 -0500

    upgrade to MonoTouch.Fabric

--- MonoTouch.Crashlytics/libFabric.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libFabric.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, Frameworks = "Security SystemConfiguration", LinkerFlags = "-lstdc++ -lz", SmartLink = true, ForceLoad = true)]
