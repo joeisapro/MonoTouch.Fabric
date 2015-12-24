d3d995295a02dc8b627529209af0675cce906bff
Joseph Miserendino <joeisapro@users.noreply.github.com>
2015-12-23 22:06:45 -0500

    cleanup

--- MonoTouch.Fabric.Crashlytics/libCrashlytics.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libCrashlytics.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, Frameworks = "Security SystemConfiguration", LinkerFlags = "-lstdc++ -lz", SmartLink = true, ForceLoad = true)]
