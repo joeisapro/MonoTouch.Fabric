ec704cda6761b328747ff87b2e0e4ca5b14cea02
Joseph Miserendino <joeisapro@users.noreply.github.com>
2015-12-23 22:07:42 -0500

    cleanup

--- MonoTouch.Fabric.MoPub/libMoPub.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libMoPub.a", LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.Arm64, Frameworks = "AVFoundation", SmartLink = true, ForceLoad = true)]
