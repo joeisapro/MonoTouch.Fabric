21941c9231d85bc7b1a2c1fcc44c5afc5e43163b
joeisapro <joeisapro@users.noreply.github.com>
2015-12-28 14:08:47 -0500

    updated twiterkitdemo

--- samples/TwitterKitDemo/TwitterKitDemo/UserTimelineViewController.cs
+++ samples/TwitterKitDemo/TwitterKitDemo/UserTimelineViewController.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using Foundation;
+using MonoTouch.Fabric.TwitterCore;
 using MonoTouch.Fabric.TwitterKit;
 
 namespace TwitteKitDemo
 {
     public class UserTimelineViewController: TWTRTimelineViewController
     {
-        public override void ViewDidLoad ()
+        public override void ViewDidLoad()
         {
-            base.ViewDidLoad ();
+            base.ViewDidLoad();
 
             var twitter = MonoTouch.Fabric.TwitterKit.Twitter.SharedInstance;
-            this.DataSource = new MonoTouch.Fabric.TwitterKit.TWTRUserTimelineDataSource ("fabric", twitter.APIClient);
+            twitter.LogInGuestWithCompletion((TWTRGuestSession session, NSError error) =>
+                {
+                    this.DataSource = new MonoTouch.Fabric.TwitterKit.TWTRUserTimelineDataSource("fabric", twitter.APIClient);
+                });
 
             this.ShowTweetActions = true;
         }
