2611d7f7194ee75a9cff906191e551fc890327fb
unknown-user <joeisapro@users.noreply.github.com>
2016-02-04 20:39:26 -0500

    fixed crashlytics binding for CLSLog and CLSNLog

--- src/MonoTouch.Fabric.Crashlytics/StructsAndEnums.cs
+++ src/MonoTouch.Fabric.Crashlytics/StructsAndEnums.cs
@@ -1,28 +1,60 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using Foundation;
+using ObjCRuntime;
 
 namespace MonoTouch.Fabric.Crashlytics
 {
-	static class CFunctions
-	{
-		// extern void CLSLog (NSString * format, ...);
-		[DllImport ("__Internal")]
-		//[Verify (PlatformInvoke)]
-		static extern void CLSLog (string format, IntPtr varArgs);
+    
+    public static class CFunctions
+    {
+        // extern void CLSLog (NSString * format, ...);
+        [DllImport("__Internal", EntryPoint = "CLSLog")]
+        //[Verify (PlatformInvoke)]
+        internal static extern void __CLSLog(IntPtr format, string arg0);
 
-		// extern void CLSLogv (NSString * format, va_list ap);
-		[DllImport ("__Internal")]
-		//[Verify (PlatformInvoke)]
-		static extern unsafe void CLSLogv (string format, sbyte* ap);
+        // extern void CLSLog (NSString * format, ...);
+        [DllImport("__Internal", EntryPoint = "CLSLog")]
+        //[Verify (PlatformInvoke)]
+        internal static extern void __CLSLog_arm64(IntPtr format, IntPtr dummy1, IntPtr dummy2, IntPtr dummy3, IntPtr dummy4, IntPtr dummy5, IntPtr dummy6, string arg0);
 
-		// extern void CLSNSLog (NSString * format, ...);
-		[DllImport ("__Internal")]
-		//[Verify (PlatformInvoke)]
-		static extern void CLSNSLog (string format, IntPtr varArgs);
+        // extern void CLSLogv (NSString * format, va_list ap);
+        [DllImport("__Internal")]
+        //[Verify (PlatformInvoke)]
+		internal static extern unsafe void CLSLogv(IntPtr format, sbyte* ap);
 
-		// extern void CLSNSLogv (NSString * format, va_list ap);
-		[DllImport ("__Internal")]
-		//[Verify (PlatformInvoke)]
-		static extern unsafe void CLSNSLogv (string format, sbyte* ap);
-	}
+        // extern void CLSNSLog (NSString * format, ...);
+        [DllImport("__Internal", EntryPoint = "CLSNSLog")]
+        //[Verify (PlatformInvoke)]
+		internal static extern void __CLSNSLog(IntPtr format, string arg0);
+
+        // extern void CLSNSLog (NSString * format, ...);
+        [DllImport("__Internal", EntryPoint = "CLSNSLog")]
+        //[Verify (PlatformInvoke)]
+        internal static extern void __CLSNSLog_arm64(IntPtr format, IntPtr dummy1, IntPtr dummy2, IntPtr dummy3, IntPtr dummy4, IntPtr dummy5, IntPtr dummy6, string arg0);
+
+        // extern void CLSNSLogv (NSString * format, va_list ap);
+        [DllImport("__Internal")]
+        //[Verify (PlatformInvoke)]
+		internal static extern unsafe void CLSNSLogv(IntPtr format, sbyte* ap);
+
+        public static void CLSLog(string message)
+        {
+            using (var nsFormat = new NSString(message))
+                if (Runtime.Arch == Arch.DEVICE && IntPtr.Size == 8)
+                    __CLSLog_arm64(nsFormat.Handle, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, "");
+                else
+                    __CLSLog(nsFormat.Handle, "");
+        }
+
+        public static void CLSNSLog(string message)
+        {
+            using (var nsFormat = new NSString(message))
+                if (Runtime.Arch == Arch.DEVICE && IntPtr.Size == 8)
+                    __CLSNSLog_arm64(nsFormat.Handle, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero, "");
+                else
+                    __CLSNSLog(nsFormat.Handle, "");
+        } 
+    }
+        
 }
